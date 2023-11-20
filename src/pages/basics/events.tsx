2e673a5d936434bca2cef661f05deb80da9d600d
Rafael Salles <rafaelsalles.sistemas@gmail.com>
2023-11-20 18:35:33 -0300

    basic event
    basic event

---
+++ src/pages/basics/events.tsx
@@ -0,0 +1,14 @@
+import Event from "@/components/basics/Event";
+
+export default function PageEvents() {
+    return (
+        <div>
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
->
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <div>
       <Primary title="Primeiro Componente" content="Seja bem vindo(a)!"  />
       </div>
