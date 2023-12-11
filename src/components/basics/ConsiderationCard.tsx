43e0dbb616482c9b4e2f7141273cbfc464f0e43f
Rafael Salles <rafaelsalles.sistemas@gmail.com>
2023-12-10 23:40:54 -0300

    tela de considerações

--- src/components/basics/ConsiderationCard.tsx
+++ src/components/basics/ConsiderationCard.tsx
@@ -1,14 +1,29 @@
+import Link from "next/link"
 
 interface ConsiderationCardProps {
     id: number
     title: string
     category: string
     content: string
-    images: []
 }
 
 export default function ConsiderationCard(props: ConsiderationCardProps) {
     return (
-         {/* Add your content and components here */}
+        <div className="flex flex-col justify-center items-center">
+            <div className="font-bold text-xl mb-2">
+                {props.title}
+            </div>
+            <div className="bg-primary text-justify text-white rounded-lg">
+                {props.category}
+            </div>
+            <div className="text-justify">
+                {props.content.substring(0, 100) + "..."}
+            </div>
+            <div className="text-blue-500 underline text-end">
+                <Link href={`/inclue/consideration?id=${props.id}`} >
+                    Saiba mais
+                </Link>
+            </div>
+        </div>
     )
 }
