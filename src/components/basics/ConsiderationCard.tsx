ab00d9a2e13412c6c881ae1eda159a57e9b5c19c
Rafael Salles <rafaelsalles.sistemas@gmail.com>
2023-12-10 21:09:56 -0300

    criação de componentes

---
+++ src/components/basics/ConsiderationCard.tsx
@@ -0,0 +1,14 @@
+
+interface ConsiderationCardProps {
+    id: number
+    title: string
+    category: string
+    content: string
+    images: []
+}
+
+export default function ConsiderationCard(props: ConsiderationCardProps) {
+    return (
+         {/* Add your content and components here */}
+    )
+}
