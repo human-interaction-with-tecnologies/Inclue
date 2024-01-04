365806283cc76faff1f9463fd9a875249180d22e
Luciano Teran <luciano.teran@icen.ufpa.br>
2024-01-04 02:59:46 -0300

    Adição do my-loader para imagens

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
   sassOptions: {
     sassOptions: {
       includePaths: [path.join(__dirname, 'styles')],
