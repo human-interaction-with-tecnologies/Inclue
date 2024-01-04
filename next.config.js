73124a6396eecdd8441e30984df097eea976180a
Luciano Teran <luciano.teran@icen.ufpa.br>
2024-01-04 03:54:14 -0300

    teste de imagem no export

--- next.config.js
+++ next.config.js
@@ -5,10 +5,8 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  trailingSlash: true,
   images: {
-    loader: 'custom',
-    loaderFile: './my-loader.ts',
+    unoptimized: true,
   },
   sassOptions: {
     sassOptions: {
